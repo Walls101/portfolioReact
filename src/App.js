51bff45197f7016076caaa896b755e17b8c0d42b
Alex Paredes <alexparedes83@gmail.com>
2023-02-10 21:14:00 -0700

    changed favicon and looking to make first page

--- src/App.js
+++ src/App.js
@@ -3,21 +3,37 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <div>
+      <header>
+        <div>
+          <h2>Paul Paredes</h2>
+        </div>
+        <div>
+          
+        </div>
       </header>
+      <body>
+
+      </body>
+      <footer>
+
+      </footer>
     </div>
   );
 }
